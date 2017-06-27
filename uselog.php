4585d51a8e181865b6de442b4b2fbac7d93bcae6
Roger Scott <theartfulroger@gmail.com>
2017-06-26 21:27:38 -0400

    Update uselog.php

--- uselog.php
+++ uselog.php
@@ -2,23 +2,23 @@
   class uselog extends db {
 
     public function __construct(
-	  $id						= null, 
-	  $action				= "",
+	  $id			= null, 
+	  $action		= "",
 	  $date_time		= "",
 	  $last_name		= "",
 	  $first_name		= "",
-	  $mem_id				= "",
-		$useragent		= "",
-		$ipaddress		= ""
+	  $mem_id		= "",
+	  $useragent		= "",
+	  $ipaddress		= ""
     ){
       parent::__construct();
       $this->table_name = "uselog";
       if($id == null){
 		$this->vars["date_time"] 	 = $date_time; 
-		$this->vars["action"]			 = $action; 
+		$this->vars["action"]		 = $action; 
 		$this->vars["last_name"]	 = $last_name; 
 		$this->vars["first_name"]	 = $first_name;	
-		$this->vars["mem_id"]			 = $mem_id;  
+		$this->vars["mem_id"]		 = $mem_id;  
 		$this->vars["useragent"]	 = $useragent;
 		$this->vars["ipaddress"]	 = $ipaddress;  		
       }else{
